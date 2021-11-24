a4eb4e97811d586b4a3b60d2adc3db7376b30d9a
posokhin-dmitry <79544402+Posokhin-Dmitry@users.noreply.github.com>
2021-11-24 21:50:38 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,70 +1,67 @@
 import React, { Component } from 'react';
-// import * as api from './Servises/api';
+import * as api from './Servises/api';
 import Searchbar from './components/Searchbar/Searchbar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
-// import Button from './components/Button/Button';
+import Button from './components/Button/Button';
 
 class App extends Component {
   state = {
-    // page: 1,
-    // dataImages: [],
-    // maxPerPage: 12,
+    page: 1,
+    dataImages: [],
+    maxPerPage: 12,
     query: '',
   };
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   const prevPage = prevState.page;
-  //   const prevQuery = prevState.query;
-  //   const newPage = this.state.page;
-  //   const newQuery = this.state.query;
-  //   const maxPerPage = this.state.maxPerPage;
+  componentDidUpdate(prevProps, prevState) {
+    const prevPage = prevState.page;
+    const prevQuery = prevState.query;
+    const newPage = this.state.page;
+    const newQuery = this.state.query;
+    const maxPerPage = this.state.maxPerPage;
 
-  //   if (prevQuery !== newQuery) {
-  //     this.setState({ page: 1, dataImages: [] });
-  //     this.searchQuery1(newPage, newQuery, maxPerPage);
-  //   }
-  //   if (prevPage !== newPage) {
-  //     this.searchQuery(newPage, newQuery, maxPerPage);
-  //   }
-  // }
+    if (prevQuery !== newQuery) {
+      this.setState({ dataImages: [], page: 1 });
+      this.searchQuery(newPage, newQuery, maxPerPage);
+      console.log('query');
+      console.log(this.state.dataImages);
+    }
+    if (prevPage !== newPage) {
+      this.nextPage(newPage, newQuery, maxPerPage);
+      console.log('page', this.state.page);
+      console.log(this.state.dataImages);
+    }
+  }
+
+  searchQuery = (page, query, maxPerPage) => {
+    api.searchQuery(page, query, maxPerPage).then(({ data }) => {
+      this.setState({ dataImages: [...data.hits] });
+    });
+  };
+
+  nextPage = (page, query, maxPerPage) => {
+    api.searchQuery(page, query, maxPerPage).then(({ data }) => {
+      this.setState({ dataImages: [...this.state.dataImages, ...data.hits] });
+    });
+  };
 
   handleFormSubmit = query => {
     this.setState({ query });
   };
 
-  // searchQuery1 = (page, query, maxPerPage) => {
-  //   api.searchQuery(page, query, maxPerPage).then(({ data }) => {
-  //     console.log(data.hits);
-  //     console.log(this.state.dataImages);
-  //     this.setState({ dataImages: [] });
-  //     const newDataImages = [...data.hits];
-  //     this.setState({ dataImages: newDataImages });
-  //   });
-  // };
-
-  // searchQuery = (page, query, maxPerPage) => {
-  //   api.searchQuery(page, query, maxPerPage).then(({ data }) => {
-  //     console.log(data.hits);
-  //     console.log(this.state.dataImages);
-  //     const newDataImages = [...this.state.dataImages, ...data.hits];
-  //     this.setState({ dataImages: newDataImages });
-  //   });
-  // };
-
-  // handleClickLoadMore = () => {
-  //   let { page } = this.state;
-  //   page += 1;
-  //   this.setState({ page });
-  // };
+  handleClickLoadMore = () => {
+    let { page } = this.state;
+    page += 1;
+    this.setState({ page });
+  };
 
   render() {
     return (
       <div className="App">
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery query={this.state.query} />
-        {/* {this.state.dataImages.length > 11 && (
+        <ImageGallery dataImages={this.state.dataImages} />
+        {this.state.dataImages.length > 11 && (
           <Button onClick={this.handleClickLoadMore} />
-        )} */}
+        )}
       </div>
     );
   }
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,68 +1,20 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import * as api from '../../Servises/api';
-import Button from '../Button/Button';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import PropTypes from 'prop-types';
 
-class ImageGallery extends Component {
-  state = {
-    page: 1,
-    dataImages: [],
-    maxPerPage: 12,
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevPage = prevState.page;
-    const prevQuery = prevProps.query;
-    const newPage = this.state.page;
-    const newQuery = this.props.query;
-    const maxPerPage = this.state.maxPerPage;
-
-    if (prevQuery !== newQuery) {
-      this.setState({ dataImages: [], page: 1 });
-      api.searchQuery(newPage, newQuery, maxPerPage).then(({ data }) => {
-        this.setState({ dataImages: [...data.hits] });
-        console.log(data);
-        console.log(this.state.dataImages);
-        console.log('query');
-      });
-    }
-    if (prevPage !== newPage) {
-      api.searchQuery(newPage, newQuery, maxPerPage).then(({ data }) => {
-        this.setState({ dataImages: [...prevState.dataImages, ...data.hits] });
-        console.log(this.state.dataImages);
-        console.log('page', this.state.page);
-      });
-    }
-  }
-
-  handleClickLoadMore = () => {
-    let { page } = this.state;
-    page += 1;
-    this.setState({ page });
-  };
-
-  render() {
-    const { dataImages } = this.state;
-    return (
-      <>
-        <ul className="ImageGallery">
-          {dataImages.map(({ id, webformatURL, tags, largeImageURL }) => (
-            <ImageGalleryItem
-              key={id}
-              tags={tags}
-              webformatURL={webformatURL}
-              largeImageURL={largeImageURL}
-            />
-          ))}
-        </ul>
-        {this.state.dataImages.length > 11 && (
-          <Button onClick={this.handleClickLoadMore} />
-        )}
-      </>
-    );
-  }
-}
+const ImageGallery = ({ dataImages }) => {
+  return (
+    <ul className="ImageGallery">
+      {dataImages.map(({ id, webformatURL, tags, largeImageURL }) => (
+        <ImageGalleryItem
+          key={id}
+          tags={tags}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+        />
+      ))}
+    </ul>
+  );
+};
 
 ImageGallery.propTypes = {
   dataImages: PropTypes.arrayOf(PropTypes.object),
